c526ee686aa720685f25440485017b14e5c03038
github-username <dhwoqls1@gmail.com>
2023-08-07 01:53:09 +0900

    레벨 계산기 완료

--- TamagotchiProject/ViewController.swift
+++ TamagotchiProject/ViewController.swift
@@ -8,12 +8,13 @@
 import UIKit
 
 enum Feed: Int {
-    case food, water
+    case food
+    case water
 }
 
 
 class ViewController: UIViewController {
-
+    
     
     @IBOutlet var mainTamaImageView: UIImageView!
     @IBOutlet var mainTamaLebel: UILabel!
@@ -23,12 +24,13 @@ class ViewController: UIViewController {
     @IBOutlet var waterCount: UILabel!
     @IBOutlet var mentTextView: UITextView!
     
-  
+    
     @IBOutlet var fooood: UITextField!
     @IBOutlet var waaater: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-      
+        
+        
     }
     
     @IBAction func foodAndWater(_ sender: UIButton) {
@@ -43,24 +45,60 @@ class ViewController: UIViewController {
                 let count = UserDefaults.standard.integer(forKey: "ff")
                 let num = count + 1
                 UserDefaults.standard.set(num, forKey: "ff")
-                print(num)
+                foodCount.text = "밥알\(UserDefaults.standard.integer(forKey: "ff"))개"
             } else {
                 let count = UserDefaults.standard.integer(forKey: "ff")
                 let num = count + Int(fooood.text!)!
                 UserDefaults.standard.set(num, forKey: "ff")
-                print(num)
+                foodCount.text = "밥알\(UserDefaults.standard.integer(forKey: "ff"))개"
             }
-            let a = UserDefaults.standard.integer(forKey: "ff")
-            print(a)
+            
         case .water :
-            let count = UserDefaults.standard.integer(forKey: "ww")
-            let num = count + 1
-            UserDefaults.standard.set(num, forKey: "ww")
+            if waaater.text == "" {
+                let countt = UserDefaults.standard.integer(forKey: "ww")
+                let numt = countt + 1
+                UserDefaults.standard.set(numt, forKey: "ww")
+                waterCount.text = "물방울 \(UserDefaults.standard.integer(forKey: "ww"))개"
+            } else {
+                let countt = UserDefaults.standard.integer(forKey: "ww")
+                let numt = countt + Int(waaater.text!)!
+                UserDefaults.standard.set(numt, forKey: "ww")
+                waterCount.text = "물방울\(UserDefaults.standard.integer(forKey: "ww"))개"
+            }
+            
         }
+        let a: Double = UserDefaults.standard.double(forKey: "ff")
+        let b: Double = UserDefaults.standard.double(forKey: "ww")
+        print("f: \(a)")
+        print("w: \(b)")
+        var levell = Int(a / 5 + b / 2)
+        print(levell)
         
-        
+        switch levell {
+        case 0 ..< 20 :
+           return level.text = "LV1"
+        case 20 ..< 30:
+            return level.text = "LV2"
+        case 30 ..< 40:
+            return level.text = "LV3"
+        case 40 ..< 50:
+            return level.text = "LV4"
+        case 50 ..< 60:
+            return level.text = "LV5"
+        case 60 ..< 70:
+            return level.text = "LV6"
+        case 70 ..< 80:
+            return level.text = "LV7"
+        case 80 ..< 90:
+            return level.text = "Lv8"
+        case 90 ..< 100:
+            return level.text = "LV9"
+        case 100... :
+            return level.text = "LV10"
+        default:
+            return level.text = "노노(얼럿예정)"
+        }
         
     }
     
 }
-
