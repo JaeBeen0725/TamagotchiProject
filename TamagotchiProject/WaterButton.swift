9135826b9c51ad1be236347552d8a0367b2d282d
github-username <dhwoqls1@gmail.com>
2023-08-21 16:39:56 +0900

    @IBDesignable & @IBInspectable

---
+++ TamagotchiProject/WaterButton.swift
@@ -0,0 +1,27 @@
+//
+//  WaterButton.swift
+//  TamagotchiProject
+//
+//  Created by Jae Oh on 2023/08/21.
+//
+
+import UIKit
+
+@IBDesignable
+class WaterButton: UIButton {
+
+    @IBInspectable var cornerRadius: CGFloat {
+        get { return layer.cornerRadius }
+        set { layer.cornerRadius = newValue }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat {
+        get {return layer.borderWidth }
+        set { layer.borderWidth = newValue }
+    }
+    @IBInspectable var boderColor: UIColor {
+        get { return UIColor(cgColor: layer.borderColor!)}
+        set { layer.borderColor = newValue.cgColor}
+    }
+
+}
